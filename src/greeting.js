df4f4c7fb6ef18232eae2747dbfc87810667fde2
MosesChang <1234512345>
2020-06-19 17:44:35 +0800

    First version

---
+++ src/greeting.js
@@ -0,0 +1,5 @@
+const arr = ["aaa", "bbb", "cccfff"] ;
+function greeting() {
+    arr.map(obj => console.log(obj)) ;
+}
+module.exports = greeting ;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const greeting = require("./greeting") ;        // is    import greeting from "./greeting";   in ES6
+greeting() ;
