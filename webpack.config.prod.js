a4181f028fe3544e26bae1372ecd266be56370ea
MosesChang <1234512345>
2020-06-25 22:47:36 +0800

    Add some extra code

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,31 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+    // filename: '[name].[hash].js',   // 分離每一個npm包需要這個，有加上西哈值
   },
+
+  // 分離每一個npm包
+  // optimization: {
+  //   runtimeChunk: 'single',
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     maxInitialRequests: Infinity,
+  //     minSize: 0,
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name(module) {
+  //           // get the name. E.g. node_modules/packageName/not/this/part.js
+  //           // or node_modules/packageName
+  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+  //           // npm package names are URL-safe, but some servers don't like @ symbols
+  //           return `npm.${packageName.replace('@', '')}`;
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
+
   module: {
     rules: [
       {
